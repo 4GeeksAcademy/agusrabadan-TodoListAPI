d27bfd2fb997f0f2e0a4dee34eb74307b3202658
Agustin Rabadán Martínez <agusrabadan85@gmail.com>
2024-04-23 09:47:01 +0000

    finish

--- src/js/component/ToDoList.jsx
+++ src/js/component/ToDoList.jsx
@@ -1,28 +1,38 @@
 import React, { useState } from "react";
 
 const ToDoList = () => {
-
-    const [input,setInput] = useState(" ")
+    const [input, setInput] = useState("");
+    const [toDo, setToDo] = useState([]);
 
     return (
-        <div>
-            <h1>Todo List</h1>
-           <ul>
-           <li>
-                <input 
-                type="text" 
-                value={input} 
-                placeholder="Add Task"
-                onChange={(e)=>setInput(e.target.value)}
-                
-                
-                >
-                </input>
-            </li>
-            <li>{input}</li>
-           </ul>
+        <div className="container mx-auto text-center">
+            <h1> Lista Asistentes</h1>
+            <ul className="list-group col-4 mx-auto mt-3">
+                <li className="list-group-item active">
+                    <input
+                        type="text"
+                        value={input}
+                        placeholder="Add Task"
+                        onChange={(event) => setInput(event.target.value)}
+                        onKeyPress={(event) => {
+                            if (event.key === "Enter" && input.trim() !== "") {
+                                setToDo([...toDo, input]);
+                                setInput("");
+                            }
+                        }}
+                    />
+                </li>
+                {toDo.map((item, index) => (
+                    <li key={index} className="list-group-item d-flex justify-content-between align-items-center">
+                        <span>{item}</span>
+                        <i className="fas fa-trash-alt" style={{ cursor: "pointer" }} onClick={() => setToDo(toDo.filter((_, currentIndex) => index !== currentIndex))}></i>
+                    </li>
+                ))}
+
+            </ul>
+            <div><strong> {toDo.length} tareas</strong></div>
         </div>
     );
 };
 
-export default ToDoList
+export default ToDoList;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import ToDoList from "./ToDoList";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
+		<div>
 			<ToDoList />
 		</div>
 	);
